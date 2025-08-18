1eb55c175b93e4be1e6afdfd044aa8cb65f77fd2
tashfeen786 <tashfeenakhonzada786@gmail.com>
2025-08-18 12:34:21 +0500

    Update why_pydantic.py

--- why_pydantic.py
+++ why_pydantic.py
@@ -1,17 +1,23 @@
+from pydantic import BaseModel
+from typing import List, Dict, Optional
 
+class patient(BaseModel):
+    name: str
+    age: int
+    weight : float
+    allergies :Optional[ List[str]]
+    contact_detalies : Dict[str, str]
 
-def insert_patient_data(name: str, age: int):
-    if name == str and age == int:
-        print(name)
-        print(age)
-        print('inserted into database')
-    else:
-        raise TypeError('Incorrect data type')
+def insert_patient_data(patient: patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.weight)
+    print(patient.allergies)
+    print(patient.contact_detalies)
+    print('inserted into database')
     
-def update_patient_data(name: str, age: int):
-    if name == str and age == int:
-        print(name)
-        print(age)
-        print('Update patient data in database')
-    else:
-        raise TypeError('Incorrect Datatype ')
+patient_info = {'name': 'Tashfeen Aziz', 'age':30, 'weight': 63.6, 'allergies': ['pollen', 'dust'], 'contact_detalies': {'email': 'abc@gmail.com', 'phone': '1234567890'}}
+
+patient1 = patient(**patient_info)
+
+insert_patient_data(patient1)
